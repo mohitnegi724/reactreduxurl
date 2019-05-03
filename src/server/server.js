cd08e17094ad95b251800b4cefee2302286a69b5
mohitnegi724 <mohitnegi724@gmail.com>
2019-05-03 07:19:53 +0530

    Link Opened Time:

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ const ids = require('short-id');
 const bodyParser = require('body-parser');
 const Links = require('./models/mongodb');
 
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,25 +16,36 @@ const Keys = require('./config/keys');
 const mongoose = require('mongoose');
 const db = Keys.mongoURI;
 mongoose.connect(db)
-    .then(() => console.log('db Connected'))
-    .catch((error) => console.log("there was an error" + error));
+    .then(success => console.log('db Connected' + success))
+    .catch(error => console.log("there was an error" + error));
 
 app.get('/', (req, res) => {
     Links.find().then(data => res.send(data)).catch(err => res.send(err));
 });
 
 
+
+//Get Redirected
 app.get('/:id', (req, res) => {
     let urlupcomingReqId = req.params.id;
-    Links.findById(urlupcomingReqId)
+    Links.findOne({_id:urlupcomingReqId})
         .then(data => {
-            Date.now() < data.expiryDate ? res.status(301).redirect(data.originalUrl) : res.status(404).send("Link expired"); 
-         }).catch(err => res.send(err));
+            if(Date.now() < data.expiryDate){
+                res.status(301).redirect(data.originalUrl);
+                Links.findOneAndUpdate({_id:urlupcomingReqId}, {$set:{linkOpened:data.linkOpened+1}})
+                .then(()=>console.log("Success"))
+                .catch(err=>console.log(err));
+            }else{
+                res.status(404).send("Link expired");
+            } 
+         }).catch(err=>res.send(err));
 });
 
+
+//Delete All Links
 app.get('/delete/all', (req, res)=>{
     Links.remove()
-    .then(success=>res.send(success))
+    .then(success=>res.send("All Clear :-)"))
     .catch(err=>res.send(err));
 });
 
@@ -45,11 +57,17 @@ app.post('/short', (req, res, next) => {
         shortUrl: ids.generate(),
         originalUrl: req.body.originalUrl,
         createdDate: Date.now(),
-        expiryDate: Date.now() + 604800000
+        expiryDate: Date.now() + (30*24*60*60*100)
     };
     Links.create(newShortUrl)
         .then(url => res.send(newShortUrl))
         .catch(err => res.send(err));
+});
+
+//Premium Service
+app.get('/premium/:id', (req, res)=>{
+    let urlupcomingReqId = req.params.id;
+    Links.findByIdAndUpdate(urlupcomingReqId, { $set: { expiryDate: 1675276200000}}).then(data=>res.send(data)).catch(err=>res.send(err));
 });
 
 
